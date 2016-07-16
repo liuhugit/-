b2dd6ba2d4a4f16b7bdcd92aae40cf76cb609e48
Mr_liu <673636634@qq.com>
2016-07-16 23:29:52 +0800

    Initial Commit

---
+++ ImagesFlex/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImagesFlex
+//
+//  Created by 刘虎 on 16/7/16.
+//  Copyright © 2016年 刘虎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImagesFlex/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImagesFlex
+//
+//  Created by 刘虎 on 16/7/16.
+//  Copyright © 2016年 刘虎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
